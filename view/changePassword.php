8f7d24c22905c3db62a26f40d3ea42860180c589
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-08-06 00:00:11 +0530

    left side nav bar removed modal for changing pass , and created change password page via twig

---
+++ view/changePassword.php
@@ -0,0 +1,21 @@
+{% extends 'partials/header.html' %}
+
+{% block body %}Change Password Panel{% endblock %}
+{% block content %}
+{{ include('partials/navigation.php') }}
+<div class="container">
+    <h1 class="text-center my-5">User Change Password</h1>
+    <form class="text-center px-5" id="myForm" action="department.php" autocomplete="off" method="POST">
+        <div class="mb-4">
+            <input name="oldpass" id="oldpass" type="password" class="form-control" placeholder="Enter The Old Password">
+        </div>
+    <div class="mb-4">
+        <input name="pass" id="pass" type="password" class="form-control" placeholder="Enter The Password">
+    </div>
+    <div class="mb-1"><span id="available"></span></div>
+    <div class="hide"><button id="submit" class="btn btn-primary">Change</button></div>
+</form>
+</div>
+<!-- <script src="../public/javascript/changePassword.js"></script> -->
+
+{% endblock %}
